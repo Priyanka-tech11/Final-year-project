7ff76e61bfc1133916b4ec9fcbb30acc6a02d2f4
Priyanka <priyankapinka21@gmail.com>
2026-04-10 18:06:07 +0530

    Update login.js

--- login.js
+++ login.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', async (e) => {
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('https://final-year-project-production-dcac.up.railway.app/login', {
+        const response = await fetch('https://final-year-project-production-dcac.up.railway.app/auth/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
